a7c7e6f58bd167c96b3fd5137ad1fa61061927fb
Lachie Kennett <lachie@coachkennett.com>
2026-04-28 15:38:34 +1000

    Fix react-markdown ESM build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['react-markdown', 'remark', 'remark-parse', 'unified', 'bail', 'is-plain-obj', 'trough', 'vfile', 'vfile-message', 'unist-util-stringify-position', 'mdast-util-from-markdown', 'mdast-util-to-string', 'micromark', 'decode-named-character-reference', 'character-entities', 'remark-rehype', 'mdast-util-to-hast', 'trim-lines', 'unist-util-is', 'unist-util-visit', 'unist-util-visit-parents', 'unist-util-position', 'rehype-raw', 'hast-util-raw', 'hast-util-from-parse5', 'hast-util-to-parse5', 'html-void-elements', 'property-information', 'space-separated-tokens', 'comma-separated-tokens', 'zwitch', 'hastscript', 'web-namespaces'],
 };
 
 export default nextConfig;
